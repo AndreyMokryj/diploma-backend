87a953f760775cc89acd17d3d4877f27713cbc76
andrew <andrew_mokryj@ukr.net>
2020-04-14 22:44:27 +0300

    Removed unused CREs

--- ControlService/src/main/java/ControlService/Repositories/MovieRepository.java
+++
@@ -1,6 +0,0 @@
-package ControlService.Repositories;
-
-import ControlService.Entities.MovieE;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepository extends CrudRepository<MovieE, Long> { }
