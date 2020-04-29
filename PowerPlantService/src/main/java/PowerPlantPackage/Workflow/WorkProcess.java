e23cdc18c9268eb9cf80b3c932bf6d56b20a3119
andrew <andrew_mokryj@ukr.net>
2020-04-29 22:21:18 +0300

    Refactor - add method 'findSun(panel)'

--- PowerPlantService/src/main/java/PowerPlantPackage/Workflow/WorkProcess.java
+++ PowerPlantService/src/main/java/PowerPlantPackage/Workflow/WorkProcess.java
@@ -16,7 +16,7 @@ public class WorkProcess {
         panels = new ArrayList<Object>();
         restTemplate = new RestTemplate();
         userId = null;
-        index = 700;
+        index = 0;
     }
 
     public static WorkProcess getInstance(){
@@ -43,126 +43,7 @@ public class WorkProcess {
                 PanelVO panel = PanelVO.fromMap((Map) object);
                 double power = getPower(panel);
                 if (power >= 10){
-                    double azPlus = 0;
-                    double azMinus = 0;
-                    double altPlus = 0;
-                    double altMinus = 0;
-
-                    while (azPlus >= 0 || azMinus >= 0 || altPlus >= 0 || altMinus >= 0) {
-                        double prevPower = getPower(panel);
-                        Random random = new Random();
-
-                        StateVO prevState = getState(panel);
-                        azPlus = prevState.getAzPlus();
-                        azMinus = prevState.getAzMinus();
-                        altPlus = prevState.getAltPlus();
-                        altMinus = prevState.getAltMinus();
-
-                        int code = 0;
-
-                        int randomInt = random.nextInt(20);
-                        if (randomInt == 5) {
-                            code = random.nextInt(4);
-                        } else {
-                            if (azPlus >= azMinus && azPlus >= altPlus && azPlus >= altMinus) {
-                                code = 2;
-                            }
-                            if (azMinus >= azPlus && azMinus >= altPlus && azMinus >= altMinus) {
-                                code = 3;
-                            }
-                            if (altPlus >= azMinus && altPlus >= azPlus && altPlus >= altMinus) {
-                                code = 0;
-                            }
-                            if (altMinus >= azMinus && altMinus >= azPlus && altMinus >= altPlus) {
-                                code = 1;
-                            }
-                        }
-
-                        PreviousVO previousVO = new PreviousVO();
-
-                        switch (code) {
-                            case 0:
-                                if (panel.getAltitude() < 90) {
-                                    panel.setAltitude(panel.getAltitude() + 1);
-                                    break;
-                                } else {
-                                    previousVO.setAltPlus(-1);
-                                    code++;
-                                }
-
-                            case 1:
-                                if (panel.getAltitude() > 5) {
-                                    panel.setAltitude(panel.getAltitude() - 1);
-                                    break;
-                                } else {
-                                    previousVO.setAltMinus(-1);
-                                    code++;
-                                }
-
-                            case 2:
-                                if (panel.getAzimuth() < 359) {
-                                    panel.setAzimuth(panel.getAzimuth() + 1);
-                                } else {
-                                    panel.setAzimuth(0);
-                                }
-                                break;
-                            case 3:
-                                if (panel.getAzimuth() > 0) {
-                                    panel.setAzimuth(panel.getAzimuth() - 1);
-                                } else {
-                                    panel.setAzimuth(359);
-                                }
-                                break;
-                        }
-
-                        StateVO newState = getState(panel);
-                        double newPower = getPower(panel);
-
-                        double diff = newPower - prevPower;
-
-                        PreviousVO currentVO = new PreviousVO();
-                        previousVO.setId(prevState.getId());
-                        currentVO.setId(newState.getId());
-
-                        double k = 10;
-                        switch (code) {
-                            case 0:
-                                previousVO.setAltPlus(diff);
-                                currentVO.setAltPlus(diff / k);
-
-                                previousVO.setAltMinus(-diff / k);
-                                currentVO.setAltMinus(-diff / k / k);
-                                break;
-                            case 1:
-                                previousVO.setAltMinus(diff);
-                                currentVO.setAltMinus(diff / k);
-
-                                previousVO.setAltPlus(-diff / k);
-                                currentVO.setAltPlus(-diff / k / k);
-                                break;
-                            case 2:
-                                previousVO.setAzPlus(diff);
-                                currentVO.setAzPlus(diff / k);
-
-                                previousVO.setAzMinus(-diff / k);
-                                currentVO.setAzMinus(-diff / k / k);
-                                break;
-                            case 3:
-                                previousVO.setAzMinus(diff);
-                                currentVO.setAzMinus(diff / k);
-
-                                previousVO.setAzPlus(-diff / k);
-                                currentVO.setAzPlus(-diff / k / k);
-                                break;
-                        }
-
-                        sendUpdate(previousVO);
-                        sendUpdate(currentVO);
-                        updatePanel(panel);
-                    }
-
-                    System.out.println("Panel " + panel.getName() + ": final azimuth: " + panel.getAzimuth() + "; altitude: " + panel.getAltitude() + "; index: " + index + "; power: " + getPower(panel));
-
+                    findSun(panel);
                 }
                 else {
                     System.out.println("No sun found");
@@ -171,6 +52,128 @@ public class WorkProcess {
             index++;
             System.out.println("Task executed on " + new Date());
         }
+    }
+
+    public void findSun(PanelVO panel) {
+        double azPlus = 0;
+        double azMinus = 0;
+        double altPlus = 0;
+        double altMinus = 0;
+
+        while (azPlus >= 0 || azMinus >= 0 || altPlus >= 0 || altMinus >= 0) {
+            double prevPower = getPower(panel);
+            Random random = new Random();
+
+            StateVO prevState = getState(panel);
+            azPlus = prevState.getAzPlus();
+            azMinus = prevState.getAzMinus();
+            altPlus = prevState.getAltPlus();
+            altMinus = prevState.getAltMinus();
+
+            int code = 0;
+
+            int randomInt = random.nextInt(20);
+            if (randomInt == 5) {
+                code = random.nextInt(4);
+            } else {
+                if (azPlus >= azMinus && azPlus >= altPlus && azPlus >= altMinus) {
+                    code = 2;
+                }
+                if (azMinus >= azPlus && azMinus >= altPlus && azMinus >= altMinus) {
+                    code = 3;
+                }
+                if (altPlus >= azMinus && altPlus >= azPlus && altPlus >= altMinus) {
+                    code = 0;
+                }
+                if (altMinus >= azMinus && altMinus >= azPlus && altMinus >= altPlus) {
+                    code = 1;
+                }
+            }
+
+            PreviousVO previousVO = new PreviousVO();
+
+            switch (code) {
+                case 0:
+                    if (panel.getAltitude() < 90) {
+                        panel.setAltitude(panel.getAltitude() + 1);
+                        break;
+                    } else {
+                        previousVO.setAltPlus(-1);
+                        code++;
+                    }
+
+                case 1:
+                    if (panel.getAltitude() > 5) {
+                        panel.setAltitude(panel.getAltitude() - 1);
+                        break;
+                    } else {
+                        previousVO.setAltMinus(-1);
+                        code++;
+                    }
+
+                case 2:
+                    if (panel.getAzimuth() < 359) {
+                        panel.setAzimuth(panel.getAzimuth() + 1);
+                    } else {
+                        panel.setAzimuth(0);
+                    }
+                    break;
+                case 3:
+                    if (panel.getAzimuth() > 0) {
+                        panel.setAzimuth(panel.getAzimuth() - 1);
+                    } else {
+                        panel.setAzimuth(359);
+                    }
+                    break;
+            }
+
+            StateVO newState = getState(panel);
+            double newPower = getPower(panel);
+
+            double diff = newPower - prevPower;
+
+            PreviousVO currentVO = new PreviousVO();
+            previousVO.setId(prevState.getId());
+            currentVO.setId(newState.getId());
+
+            double k = 10;
+            switch (code) {
+                case 0:
+                    previousVO.setAltPlus(diff);
+                    currentVO.setAltPlus(diff / k);
+
+                    previousVO.setAltMinus(-diff / k);
+                    currentVO.setAltMinus(-diff / k / k);
+                    break;
+                case 1:
+                    previousVO.setAltMinus(diff);
+                    currentVO.setAltMinus(diff / k);
+
+                    previousVO.setAltPlus(-diff / k);
+                    currentVO.setAltPlus(-diff / k / k);
+                    break;
+                case 2:
+                    previousVO.setAzPlus(diff);
+                    currentVO.setAzPlus(diff / k);
+
+                    previousVO.setAzMinus(-diff / k);
+                    currentVO.setAzMinus(-diff / k / k);
+                    break;
+                case 3:
+                    previousVO.setAzMinus(diff);
+                    currentVO.setAzMinus(diff / k);
+
+                    previousVO.setAzPlus(-diff / k);
+                    currentVO.setAzPlus(-diff / k / k);
+                    break;
+            }
+
+            sendUpdate(previousVO);
+            sendUpdate(currentVO);
+            updatePanel(panel);
+        }
+
+        System.out.println("Panel " + panel.getName() + ": final azimuth: " + panel.getAzimuth() + "; altitude: " + panel.getAltitude() + "; index: " + index + "; power: " + getPower(panel));
     }
 
     public double getPower(PanelVO panel){
