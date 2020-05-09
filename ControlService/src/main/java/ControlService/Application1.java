71d689699d6e43b150f432b1085da66279370fb8
andrew <andrew_mokryj@ukr.net>
2020-05-09 14:55:58 +0300

    Add Eureka registration for ControlService and PowerPlantService

--- ControlService/src/main/java/ControlService/Application1.java
+++ ControlService/src/main/java/ControlService/Application1.java
@@ -2,7 +2,9 @@ package ControlService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application1 {
     public static void main(String[] args) throws Exception {
--- PowerPlantService/src/main/java/PowerPlantPackage/Application1.java
+++ PowerPlantService/src/main/java/PowerPlantPackage/Application1.java
@@ -3,6 +3,7 @@ package PowerPlantPackage;
 import PowerPlantPackage.Workflow.WorkProcess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.TimerTask;
@@ -10,6 +11,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class Application1 {
