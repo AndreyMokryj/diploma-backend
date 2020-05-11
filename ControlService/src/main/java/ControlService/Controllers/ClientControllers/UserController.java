eda43785651c10ef2c273ea9f7171ca627328c8c
andrew <andrew_mokryj@ukr.net>
2020-05-11 15:09:46 +0300

    Set UserController request mapping to /app/users

--- ControlService/src/main/java/ControlService/Controllers/ClientControllers/UserController.java
+++ ControlService/src/main/java/ControlService/Controllers/ClientControllers/UserController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(path="/users")
+@RequestMapping(path="/app/users")
 @Component
 public class UserController {
     @Autowired
